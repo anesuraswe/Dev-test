0a39c007ccb8084e084eb9f3dd71373ab5e56f88
anesuraswe <ane.uraswe@gmail.com>
2024-06-19 12:35:54 +0200

    initial stage

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Services from './components/Services'
+
+function App() {
+  
+
+  return (
+    <>
+     
+     <Services/>
+     
+    
+    </>
+  )
+}
+
+export default App
