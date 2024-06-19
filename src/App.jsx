2d87ac897b1953e2be655ae9d642c865445a8081
anesuraswe <ane.uraswe@gmail.com>
2024-06-19 20:32:27 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import EmployeeForm from './components/EmployeeForm';
-import EmployeeSearch from './components/EmployeeSearch';
-import EmployeeHistory from './components/EmployeeHistory';
+import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import EmployeeForm from "./components/EmployeeForm";
+import EmployeeSearch from "./components/EmployeeSearch";
+import EmployeeHistory from "./components/EmployeeHistory";
 
 const App = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/add-employee" element={<EmployeeForm/>} />
-                <Route path="/search-employees" element={<EmployeeSearch/>} />
-                <Route path="/employee-history/:id" element={<EmployeeHistory/>} />
-                <Route path="/" element={<EmployeeSearch/>} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <>
+      <EmployeeForm />
+      <EmployeeSearch />
+      <EmployeeHistory />
+      <EmployeeSearch />
+    </>
+  );
 };
 
 export default App;
